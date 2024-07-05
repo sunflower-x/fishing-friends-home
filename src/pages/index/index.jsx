e45933f9648687421642ba1a82cb099ac8fca7ac
duruo <duruo@dianjia.io>
2024-07-05 17:30:51 +0800

    'feat:备份’

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,7 +2,9 @@ import { Component, useEffect, useState } from "react";
 import { View, Text, Map } from "@tarojs/components";
 import { observer, inject } from "mobx-react";
 import BaseModel from "./model";
-import { Tabs, Tab, Swiper, SwiperItem,Image,Icon  } from "@antmjs/vantui";
+// import { Tabs, Tab, Swiper, SwiperItem,Image,Icon,Button  } from "@antmjs/vantui";
+import { Tabs,Button,Image } from 'antd-mobile'
+import { RightOutline,LocationFill } from 'antd-mobile-icons'
 import "./index.scss";
 
 const baseModel = new BaseModel();
@@ -10,7 +12,7 @@ const { indexBase } = baseModel;
 const appid = "wx383b938a22e63de6";
 const secret = "&grant_type=authorization_code";
 const App = () => {
-  const [code, setCode] = useState("");
+  // const [code, setCode] = useState("");
   const images = [
     'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
     'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg',
@@ -92,69 +94,17 @@ const App = () => {
   };
   return (
     <>
-     <Swiper
-        height={200}
-        paginationColor="#426543"
-        autoPlay="3000"
-        initPage={0}
-        paginationVisible
-      >
-        {images.map((item, index) => (
-          <SwiperItem key={`swiper#demo1${index}`}>
-            <Image src={item} fit="cover" width="100%" height={`200px`} />
-          </SwiperItem>
-        ))}
-      </Swiper>
-      <Tabs  animated swipeable>
-        <Tab title="钓点">
-          <div className="tab-wrapper">
-            <div className="card">
-              {/* 头部 */}
-              <div className="card-header">
-                <div>标题</div>
-                <div>钓点详情 <Icon name="arrow" size="14px" className="icon"/></div>
-              </div>
-              {/* 地点 */}
-              <div className="card-location">
-                <div><Icon name="location" size="18px" className="icon"/>江西省乐平市瑞昌市小雨村</div><span>(300m)</span>
-              </div>
-              {/* 内容 */}
-              <div className="card-content">
-                <div className="localtion-wrapper">
-                  <div className="location-intro">这个位置非常不错，666，8888，939393991929929293333333333333333333333333333333333333333333333333333333333331111</div>
-                  <div>发布人：</div>
-                  <div>发布时间：</div>
-                </div>
-                <Image src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg" style={{width:'100px',height:'100px'}} fit="cover" alt="图片" />
-              </div>
-              {/* 底部 */}
-            </div>
-          </div>
-        </Tab>
-        <Tab title="天气">
-          <div className="tab-wrapper"></div>
-        </Tab>
-        <Tab title="科普">
-          <div className="tab-wrapper"></div>
-        </Tab>
-        <Tab title="钓友">
-          <div className="tab-wrapper"></div>
-        </Tab>
-      </Tabs>
-      {/* <Button onClick={() => { indexBase.getUserInfo() }}>发送请求</Button>
-        <Button onClick={handleLogin}>登录</Button>
-        <Button onClick={handleCheckLogin}>检查登录状态是否过期</Button> */}
-      {/* <Map
-        style={{ width: "100%", height: "300px" }}
-        scale={10}
-        longitude={120.29977798461914}
-        latitude={30.27289990234375}
-        showLocation={true}
-        showScale={true}
-        onClick={() => {
-          console.log(1111);
-        }}
-      /> */}
+      <Tabs defaultActiveKey='vegetables'>
+          <Tabs.Tab title='水果' key='fruits'>
+            菠萝
+          </Tabs.Tab>
+          <Tabs.Tab title='蔬菜' key='vegetables'>
+            西红柿
+          </Tabs.Tab>
+          {/* <Tabs.Tab title='动物' key='animals'>
+            蚂蚁
+          </Tabs.Tab> */}
+        </Tabs>
     </>
   );
 };
